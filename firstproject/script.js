3e43b09de111933d99ecb574207654bc2eb45fe5
Azeem <azeem999750@gmail.com>
2026-09-22 21:31:38 +0530

    Add dynamic task creation

--- firstproject/script.js
+++ firstproject/script.js
@@ -1,6 +1,48 @@
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskForm = document.getElementById("taskForm");
 
+const saveTaskBtn = document.getElementById("saveTaskBtn");
+const subjectInput = document.getElementById("subjectInput");
+const taskInput = document.getElementById("taskInput");
+
+const taskSection = document.querySelector(".task-section");
+
+
+// Show Task Form
 addTaskBtn.addEventListener("click", function () {
     taskForm.style.display = "block";
+});
+
+
+// Save Task
+saveTaskBtn.addEventListener("click", function () {
+
+    const subject = subjectInput.value;
+    const task = taskInput.value;
+
+    if (subject === "" || task === "") {
+        alert("Please enter subject and task.");
+        return;
+    }
+
+    // Create Task Card
+    const taskCard = document.createElement("div");
+
+    taskCard.classList.add("task-card");
+
+    taskCard.innerHTML = `
+        <h3>${subject}</h3>
+        <p>${task}</p>
+        <input type="checkbox" class="task-checkbox">
+    `;
+
+    // Add task to page
+    taskSection.appendChild(taskCard);
+
+    // Clear inputs
+    subjectInput.value = "";
+    taskInput.value = "";
+
+    // Hide form
+    taskForm.style.display = "none";
 });

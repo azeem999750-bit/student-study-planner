335af3ab9c1773d18085f823207ac7bae8223b34
Azeem <azeem999750@gmail.com>
2026-09-22 21:28:10 +0530

    Add task form and button

---
+++ firstproject/script.js
@@ -0,0 +1,6 @@
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskForm = document.getElementById("taskForm");
+
+addTaskBtn.addEventListener("click", function () {
+    taskForm.style.display = "block";
+});
